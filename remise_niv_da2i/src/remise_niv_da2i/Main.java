e548c6b98566200ce4cfd900440e6aee932a43a1
lamarqux <lamarqux@meleze11.iut-infobio.priv.univ-lille1.fr>
2016-09-16 10:53:29 +0200

    test_4

--- remise_niv_da2i/src/remise_niv_da2i/Main.java
+++ remise_niv_da2i/src/remise_niv_da2i/Main.java
@@ -23,7 +23,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello!");
+        System.out.println("hello world!");
     }
     
 }
