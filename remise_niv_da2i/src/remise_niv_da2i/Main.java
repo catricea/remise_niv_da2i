39cac3c0114133da492e954dde2901c4780e3be1
Mohammed LAREDJ <laredjm@meleze19.iut-infobio.priv.univ-lille1.fr>
2016-09-16 11:34:54 +0200

    test_6

--- remise_niv_da2i/src/remise_niv_da2i/Main.java
+++ remise_niv_da2i/src/remise_niv_da2i/Main.java
@@ -36,7 +36,7 @@ public class Main {
  * @param args
  */
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("algerkov");
     }
     
 }
